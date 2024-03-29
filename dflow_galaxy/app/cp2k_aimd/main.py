e1fe15719b9da658899a0e6fa8d75fd0dfabd477
weihong.xu <xuweihong.cn@gmail.com>
2024-03-29 09:39:36 +0800

    wip: build setup app

--- dflow_galaxy/app/cp2k_aimd/main.py
+++ dflow_galaxy/app/cp2k_aimd/main.py
@@ -1,5 +1,5 @@
 from dp.launching.typing import BaseModel, Field, OutputDirectory, InputFilePath
-from dp.launching.typing import Int, String, Enum, Float
+from dp.launching.typing import Int, String, Enum, Float, Boolean
 from dp.launching.cli import to_runner, default_minimal_exception_handler
 
 
@@ -13,14 +13,52 @@ class AccuracyOptions(String, Enum):
     medium = "medium"
     high = "high"
 
+
 class BasicSetOptions(String, Enum):
-    ...
+    HFX_BASIS = "HFX_BASIS"
+    BASIS_ADMM = "BASIS_ADMM"
+    BASIS_def2_QZVP_RI_ALL = "BASIS_def2_QZVP_RI_ALL"
+    BASIS_MOLOPT_LnPP1 = "BASIS_MOLOPT_LnPP1"
+    BASIS_MOLOPT_AcPP1 = "BASIS_MOLOPT_AcPP1"
+    BASIS_SET = "BASIS_SET"
+    BASIS_MOLOPT_UZH = "BASIS_MOLOPT_UZH"
+    BASIS_ADMM_ae = "BASIS_ADMM_ae"
+    ALL_BASIS_SETS = "ALL_BASIS_SETS"
+    BASIS_LRIGPW_AUXMOLOPT = "BASIS_LRIGPW_AUXMOLOPT"
+    BASIS_MOLOPT = "BASIS_MOLOPT"
+    BASIS_MINIX = "BASIS_MINIX"
+    BASIS_MOLOPT_UCL = "BASIS_MOLOPT_UCL"
+    BASIS_ZIJLSTRA = "BASIS_ZIJLSTRA"
+    GTH_BASIS_SETS = "GTH_BASIS_SETS"
+    EMSL_BASIS_SETS = "EMSL_BASIS_SETS"
+    BASIS_MINBAS = "BASIS_MINBAS"
+    BASIS_ccGRB_UZH = "BASIS_ccGRB_UZH"
+    BASIS_ADMM_MOLOPT = "BASIS_ADMM_MOLOPT"
+    BASIS_ADMM_UZH = "BASIS_ADMM_UZH"
+    BASIS_pob = "BASIS_pob"
+    BASIS_PERIODIC_GW = "BASIS_PERIODIC_GW"
+    BASIS_RI_cc_TZ = "BASIS_RI_cc-TZ"
+    BASIS_MOLOPT_LnPP2 = "BASIS_MOLOPT_LnPP2"
+
 
 class PotentialOptions(String, Enum):
-    ...
+    GTH_SOC_POTENTIALS = "GTH_SOC_POTENTIALS"
+    LnPP2_POTENTIALS = "LnPP2_POTENTIALS"
+    HF_POTENTIALS = "HF_POTENTIALS"
+    GTH_POTENTIALS = "GTH_POTENTIALS"
+    ALL_POTENTIALS = "ALL_POTENTIALS"
+    ECP_POTENTIALS_pob_TZVP_rev2 = "ECP_POTENTIALS_pob-TZVP-rev2"
+    ECP_POTENTIALS = "ECP_POTENTIALS"
+    NLCC_POTENTIALS = "NLCC_POTENTIALS"
+    AcPP1_POTENTIALS = "AcPP1_POTENTIALS"
+    LnPP1_POTENTIALS = "LnPP1_POTENTIALS"
 
 
 class InputArgs(BaseModel):
+    dry_run: Boolean = Field(
+        default = True,
+        description="Generate configuration file without running the simulation")
+
     system_file: InputFilePath = Field(
         description="Upload a system file as the initial structure of AIMD simulation")
 
@@ -44,9 +82,33 @@ class InputArgs(BaseModel):
         default=0.5,
         description="Time step of the simulation in fs")
 
+    basis_set: BasicSetOptions = Field(
+        default=BasicSetOptions.BASIS_MOLOPT,
+        description='Select the basis set for the simulation')
+
+    potential: PotentialOptions = Field(
+        default=PotentialOptions.GTH_POTENTIALS,
+        description='Select the potential for the simulation')
+
     raw_cp2k_input: InputFilePath = Field(
-        description='Advance option, provide your own CP2K input file, note that if you provide this file, the above settings will be ignored.')
+        description='Provide your own CP2K input file, note that the above settings will be ignored.')
 
 
-def main():
-    ...
+def main_entry(args: InputArgs) -> int:
+    status = 0
+    from pprint import pprint
+    pprint(args.dict())
+    return status
+
+
+def to_parser():
+    return to_runner(
+        InputArgs,
+        main_entry,
+        version="0.1.0",
+        exception_handler=default_minimal_exception_handler
+    )
+
+if __name__ == "__main__":
+    import sys
+    to_parser()(sys.argv[1:])
