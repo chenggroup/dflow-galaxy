269ea3ce25242de80719cd9571950d64462928ad
weihong.xu <xuweihong.cn@gmail.com>
2024-04-07 21:34:16 +0800

    wip: dynacat tesla

--- dflow_galaxy/app/dynacat_tesla/main.py
+++ dflow_galaxy/app/dynacat_tesla/main.py
@@ -2,7 +2,7 @@ from dp.launching.typing import BaseModel, Field, OutputDirectory, InputFilePath
 from dp.launching.typing import Int, String, Enum, Float, Boolean
 from dp.launching.cli import to_runner, default_minimal_exception_handler
 
-from dflow_galaxy.app.common import DFlowOptions, setup_dflow_context
+from dflow_galaxy.app.common import DFlowOptions, setup_dflow_context, EnsembleOptions
 from dflow_galaxy.res import get_cp2k_data_dir
 from dflow_galaxy.core.log import get_logger
 from ai2_kit.feat import catalysis as ai2cat
@@ -15,4 +15,25 @@ import sys
 logger = get_logger(__name__)
 
 
+class DynacatTeslaArgs(DFlowOptions):
 
+    dry_run: Boolean = Field(
+        default = True,
+        description="Generate configuration file without running the simulation")
+
+
+def launch_app(args: DynacatTeslaArgs) -> int:
+    setup_dflow_context(args)
+    return 0
+
+
+def main():
+    to_runner(
+        DynacatTeslaArgs,
+        launch_app,
+        version="0.1.0",
+    )(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    main()
