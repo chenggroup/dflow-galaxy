d4145b4902b9c71194be4846b37f8351185f731e
weihong.xu <xuweihong.cn@gmail.com>
2024-04-07 09:38:42 +0800

    wip: implement dynacat_md

--- dflow_galaxy/app/dynacat_md/main.py
+++ dflow_galaxy/app/dynacat_md/main.py
@@ -1,4 +1,4 @@
-from dp.launching.typing import BaseModel, Field, OutputDirectory, InputFilePath, Optional
+from dp.launching.typing import BaseModel, Field, OutputDirectory, InputFilePath, Optional, Dict
 from dp.launching.typing import Int, String, Enum, Float, Boolean
 from dp.launching.cli import to_runner, default_minimal_exception_handler
 
@@ -6,6 +6,8 @@ from dflow_galaxy.app.common import DFlowOptions, setup_dflow_context
 from dflow_galaxy.res import get_cp2k_data_dir
 from dflow_galaxy.core.log import get_logger
 from ai2_kit.feat import catalysis as ai2cat
+from ai2_kit.core.util import dump_text, dump_json
+
 
 from pathlib import Path
 import shutil
@@ -13,6 +15,18 @@ import sys
 
 
 logger = get_logger(__name__)
+
+class EnsableOptions(String, Enum):
+    nvt = 'nvt'
+    nvt_i = 'nvt-i'
+    nvt_a = 'nvt-a'
+    nvt_iso = 'nvt-iso'
+    nvt_aniso = 'nvt-aniso'
+    npt = 'npt'
+    npt_t = 'npt-t'
+    npt_tri = 'npt-tri'
+    nve = 'nve'
+    csvr = 'csvr'
 
 
 class DynaCatMdArgs(DFlowOptions):
@@ -24,13 +38,9 @@ class DynaCatMdArgs(DFlowOptions):
     system_file: InputFilePath = Field(
         description="A system file as the initial structure of LAMMPS simulation")
 
-    steps: Int = Field(
-        default=20000,
-        description='Steps of LAMMPS simulation')
-
-    step_size: Float = Field(
-        default=0.0005,
-        description='Time step size of LAMMPS simulation')
+    ensemble: EnsableOptions = Field(
+        default=EnsableOptions.csvr,
+        description='Ensemble of LAMMPS simulation')
 
     temperature: Float = Field(
         default=330,
@@ -40,8 +50,72 @@ class DynaCatMdArgs(DFlowOptions):
         format='multi-line',
         description='Plumed configuration file for metadynamics simulation')
 
+    pressure: Float = Field(
+        default=-1,
+        description='Pressure of LAMMPS simulation, should be -1 unless it is NPT ensemble')
+
+    steps: Int = Field(
+        default=20000,
+        description='Steps of LAMMPS simulation')
+
+    step_size: Float = Field(
+        default=0.0005,
+        description='Time step size of LAMMPS simulation')
+
+    sample_freq: Int = Field(
+        title='Sampling Frequency',
+        default=10,
+        description='Sampling frequency of LAMMPS simulation')
+
+    other_args: Dict[String, Float] = Field(
+        title='Other Arguments',
+        default={
+            'tau_t': 0.1,
+            'tau_p': 0.5,
+            'time_const': 0.1,
+        },
+        description="Other arguments for LAMMPS simulation, e.g. tau_t, tau_p, time_const, etc. Don't add extra arguments if you are not sure about it."
+    )
+
+    output_dir : OutputDirectory = Field(
+        default='./output',
+        description="Output directory of LAMMPS simulation")
+
 
 def launch_app(args: DynaCatMdArgs) -> int:
+    config_builder = ai2cat.ConfigBuilder()
+
+    # kwargs = {
+    #     'nsteps': 20000,
+    #     'stepsize': 0.0005,
+    #     'temp': 330,
+    #     'sample_freq': 10,
+    #     'pres': -1,
+    #     'tau_t': 0.1,
+    #     'tau_p': 0.5,
+    #     'time_const': 0.1,
+    #     'model_devi_out': 'model_devi.out',
+    #     'dump_out': 'traj.lammpstrj',
+    #     'energy_out': 'energy.log',
+    #     'data_file': 'lammps.dat',
+    #     'lammps_file': 'lammps.inp',
+    #     'plumed_file': 'plumed.inp',
+    #     'plumed_out': 'plumed.out',
+    # }
+
+    dump_text(args.plumed_config, 'plumed.inp')
+    config_builder.load_system(args.system_file).gen_lammps_input(
+        out_dir=args.output_dir,
+        nsteps=args.steps,
+        temp=args.temperature,
+        sample_freq=args.sample_freq,
+        pres= args.pressure,
+        **args.other_args,
+    )
+
+    if args.dry_run:
+        return 0
+
     return 0
 
 
